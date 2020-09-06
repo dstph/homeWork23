f7e9dc9c204fd9d8d1e6747db1093e14a9a5c918
dstph <genyadstph@gmail.com>
2020-09-06 13:01:00 +0400

    yeah

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
 Vue.component('part',{
+	data(){
+		return{
+			height: '100'
+		}
+	},
 	props: ['color'],
-	// computed: {
-	// 	style(){
-	// 		return `height: ${this.$el.children[1].value}`
-	// 	}
-	// },
+	computed: {
+ 				computedHeight: function () {
+      				return this.height;
+   				}
+	},
 	methods: {
 		onChange(){
-			console.log(this.$el.children[1].value)
+			console.log(this.$el.children[1].value);
+			this.height = this.$el.children[1].value;
+			computedHeight();
 		}
 	},
 	template: `<div class="example">
-				<div  class="slider" :class="color"></div>
+				<div  v-bind:style="{ height: computedHeight + 'px'}" class="slider" :class="color"></div>
 				<input @change="onChange" type="range" min="100" max="300">
 			</div>`
 })
 let equlaizer = new Vue({
 	el: '#app',
-	data: {
-	
-	}
-
-
 })
